b25cbb4df4d648f10943f11f7413f4ef33ffa318
Mai Trung Duc <maitrungduc1410@gmail.com>
2020-02-26 09:34:01 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+function scpFromLocal(src: string, dest: string | object, recursive: boolean) {
+
+}
+
+function scpFromRemote(dest: string, src: string, recursive: boolean) {
+
+}
+
+function mkdirRemote(dest: string) {
+
+}
+
+function deleteRemote(dest: string, recursive: boolean) {
+
+}
+
+function copyRemote(remoteSrc: string, remoteDest: string, recursive: boolean) {
+
+}
+
+function moveRemote(remoteSrc: string, remoteDest: string, recursive: boolean) {
+
+}
